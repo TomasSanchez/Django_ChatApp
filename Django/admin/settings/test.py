2f6119c93ae4cd9eb2e2c1f709b2fd19f873c66c
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-07-29 10:34:26 -0300

    Removed env

---
+++ Django/admin/settings/test.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="ZSwh29OVnr4ZeUqNEtAfyJlcQjquah1NMLNED1jMUiM1iWC45BcDutPUgN3qt8En",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
